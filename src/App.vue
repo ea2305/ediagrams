9e8e57dc2d1af8fcf4a6fed0736d365042bfc635
Elihu Cruz <eaca230596@gmail.com>
2022-06-20 23:08:25 -0500

    feat: canvas html setup

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 
 <template>
-  <ToolBar/>
+  <ToolBar />
+  <Drawer />
 </template>
 
 <script setup lang="ts">
   import ToolBar from './components/ToolBar.vue'
+  import Drawer from './components/Drawer.vue'
 </script>
---
+++ src/components/Drawer.vue
@@ -0,0 +1,28 @@
+<template>
+  <canvas :id="canvas.id" :width="canvas.width" :height="canvas.height"></canvas>
+</template>
+
+<script setup lang="ts">
+  import { onMounted, onBeforeUnmount } from 'vue'
+
+  // TODO find a better way to handle canvas configuration
+  const canvas = {
+    id: "canvas-board",
+    width: 500,
+    height: 500
+  }
+
+  onMounted((): void => {
+    console.log('[init] canvas');
+  })
+
+  onBeforeUnmount((): void => {
+    console.log('[destroy] canvas');
+  })
+</script>
+
+<style scoped>
+canvas {
+  border: 1px solid gray;
+}
+</style>
